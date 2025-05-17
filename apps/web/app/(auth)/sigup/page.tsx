b8834bb47cc0a0ccf5491f0756352e386968be66
Herman <hermanrjh@gmail.com>
2025-05-17 15:43:12 +0300

    ajout userpage

--- apps/web/app/(auth)/sigup/page.tsx
+++ apps/web/app/(auth)/sigup/page.tsx
@@ -1,7 +1,9 @@
+import SignUp from "@/components/auth/signin/Signin";
+
 export default function SignupPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-center text-4xl my-6">Signup Page</h1>
+      <SignUp/>
     </div>
   );
 }
