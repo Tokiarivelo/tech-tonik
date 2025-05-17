6fd3366fe5c27a4005774e0b50b452d4575a2999
Hasina <nyhasinafanonerantsoa@gmail.com>
2025-05-17 15:28:49 +0300

    theme

---
+++ apps/web/components/navbar/Button.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+// import Login from "../auth/login/Login"
+import { Button } from "../ui/button"
+
+const MyButton = () => {
+  return (
+    <Button variant='outline' className="mr-20" asChild>
+        <Link href='/login' className="text-white bg-secondary rounded-2xl p-3">
+            Login
+        </Link>
+    </Button>
+  )
+}
+
+export default MyButton
